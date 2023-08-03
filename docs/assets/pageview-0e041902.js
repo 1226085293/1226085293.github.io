d2c165b0c2410818cdf24d9b2df7af02cdf6c0df
yuanfengxin <1226085293@qq.com>
2023-08-03 13:40:27 +0800

    新的版本

---
+++ docs/assets/pageview-0e041902.js
@@ -0,0 +1,1 @@
+import{R as e}from"./app-4c327354.js";const a=()=>e({serverURL:"https://waline-comment.vuejs.press"});export{a as updatePageview};
--- docs/assets/pageview-ec349490.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./app-9edc1967.js";const a=()=>e({serverURL:"https://waline-comment.vuejs.press"});export{a as updatePageview};
