2925b9fb9d63fa967f71b11b0fad807e98deccc1
1226085293 <45589707+1226085293@users.noreply.github.com>
2023-09-04 07:36:21 +0000

    Deploying to site from @ 1226085293/1226085293.github.io@9f6facf619ac310214f9c02d8c6bead30627d28f 🚀

---
+++ assets/写好函数参数，避免代码修改次数.html-f3677427.js
@@ -0,0 +1,16 @@
+import{_ as n,r as i,o as a,c as s,e as d,a as r,f as t}from"./app-fbf860b5.js";const l={},c=t(`<h3 id="方式-1" tabindex="-1"><a class="header-anchor" href="#方式-1" aria-hidden="true">#</a> # 方式 1</h3><div class="language-auto line-numbers-mode" data-ext="auto"><pre class="language-auto"><code>function test(必选参数: number, 可选参数?: TestConfig): void {
+    let 参数 = new TestConfig(可选参数);
+}
+
+class TestConfig {
+    constructor(init_?: TestConfig) {
+        Object.assign(this, init_);
+    }
+    参数a?: number;
+    参数b? = &quot;str&quot;;
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="方式-2" tabindex="-1"><a class="header-anchor" href="#方式-2" aria-hidden="true">#</a> # 方式 2</h3><div class="language-auto line-numbers-mode" data-ext="auto"><pre class="language-auto"><code>// 类型声明：{ 参数a = null as number, 参数b =  &quot;str&quot; }: 类型
+function test(必选参数: number, { 参数a = null as number, 参数b =  &quot;str&quot; }): void {
+    let a = 参数a;
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><hr><h3>📣 觉得很赞？分享给你的朋友吧！</h3>`,6);function o(u,v){const e=i("Share");return a(),s("div",null,[d(" more "),c,r(e,{services:"qq,qrcode",colorful:""})])}const _=n(l,[["render",o],["__file","写好函数参数，避免代码修改次数.html.vue"]]);export{_ as default};
