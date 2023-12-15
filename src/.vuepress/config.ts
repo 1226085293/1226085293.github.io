29d8f6c49ca6f18796605efa1f83f8c643c02f5e
yuanfengxin <1226085293@qq.com>
2023-12-15 14:24:31 +0800

    添加 "分享一些有用的 ESlint 规则"

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -35,7 +35,7 @@ export default defineUserConfig({
 		// 百度网站所有权验证
 		["meta", { name: "baidu-site-verification", content: "codeva-uFXbzZZKAp" }],
 		// 防止缓存主页，缓存会导致内容不是最新的
-		["meta", { "http-equiv": "Cache-Control", content: "no-cache, no-store, must-revalidate" }],
+		["meta", { "http-equiv": "Cache-Control", content: "no-cache, no-store, must-revalidate, max-age=0" }],
 		["meta", { "http-equiv": "Pragma", content: "no-cache" }],
 		["meta", { "http-equiv": "Expires", content: "0" }],
 	],
