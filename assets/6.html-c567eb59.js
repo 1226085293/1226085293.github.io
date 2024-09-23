ca04409b8cf4470a531928209004daf60b4c606f
1226085293 <45589707+1226085293@users.noreply.github.com>
2024-09-23 04:59:46 +0000

    Deploying to site from @ 1226085293/1226085293.github.io@233f0ad938073fc3a36b42cc54a569ca537af360 🚀

---
+++ assets/6.html-c567eb59.js
@@ -0,0 +1,16 @@
+import{_ as n,o as s,c as a,e,f as t}from"./app-647ba2d7.js";const p={},o=t(`<p>设置双重进入锁判断，例：</p><div class="language-cpp line-numbers-mode" data-ext="cpp"><pre class="language-cpp"><code><span class="token keyword">bool</span> enter <span class="token operator">=</span> <span class="token boolean">false</span><span class="token punctuation">;</span>
+
+<span class="token keyword">void</span> <span class="token function">test</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">if</span> <span class="token punctuation">(</span>enter<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token keyword">return</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    mutex<span class="token punctuation">.</span><span class="token function">lock</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token keyword">if</span> <span class="token punctuation">(</span>enter<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token keyword">return</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    enter <span class="token operator">=</span> <span class="token boolean">true</span><span class="token punctuation">;</span>
+    mutex<span class="token punctuation">.</span><span class="token function">unlock</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token comment">// 代码块..</span>
+    enter <span class="token operator">=</span> <span class="token boolean">false</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2);function c(l,i){return s(),a("div",null,[e(" more "),o])}const r=n(p,[["render",c],["__file","6.html.vue"]]);export{r as default};
