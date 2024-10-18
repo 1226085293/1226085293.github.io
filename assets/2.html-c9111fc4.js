046426e946a5f108a34a72e6807bda4a1b0946e1
1226085293 <45589707+1226085293@users.noreply.github.com>
2024-10-18 09:37:08 +0000

    Deploying to site from @ 1226085293/1226085293.github.io@c57b144b07659d005afcc9e4ef948671de694614 🚀

---
+++ assets/2.html-c9111fc4.js
@@ -0,0 +1,26 @@
+import{_ as s,o as n,c as a,e,a as p,f as t}from"./app-6aabfa3f.js";const c={},o=p("p",null,"在前面演示了一个注册引导步骤的方法，如果有很多引导步骤呢？",-1),l=t(`<h2 id="示例" tabindex="-1"><a class="header-anchor" href="#示例" aria-hidden="true">#</a> 示例</h2><p>原本的步骤</p><div class="language-typescript line-numbers-mode" data-ext="ts"><pre class="language-typescript"><code><span class="token keyword">class</span> <span class="token class-name">guide_step_1</span> <span class="token keyword">extends</span> <span class="token class-name">mk</span><span class="token punctuation">.</span>guide_step_base <span class="token punctuation">{</span>
+    step_n <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+    <span class="token function">load</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span><span class="token operator">...</span><span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+
+
+<span class="token keyword">class</span> <span class="token class-name">guide_step_2</span> <span class="token keyword">extends</span> <span class="token class-name">mk</span><span class="token punctuation">.</span>guide_step_base <span class="token punctuation">{</span>
+    step_n <span class="token operator">=</span> <span class="token number">2</span><span class="token punctuation">;</span>
+    <span class="token function">load</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span><span class="token operator">...</span><span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>在 1、2 之间插入 3</p><div class="language-typescript line-numbers-mode" data-ext="ts"><pre class="language-typescript"><code><span class="token keyword">class</span> <span class="token class-name">guide_step_1</span> <span class="token keyword">extends</span> <span class="token class-name">mk</span><span class="token punctuation">.</span>guide_step_base <span class="token punctuation">{</span>
+    step_n <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+    <span class="token function">load</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span><span class="token operator">...</span><span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+
+
+<span class="token keyword">class</span> <span class="token class-name">guide_step_2</span> <span class="token keyword">extends</span> <span class="token class-name">mk</span><span class="token punctuation">.</span>guide_step_base <span class="token punctuation">{</span>
+    step_n <span class="token operator">=</span> <span class="token number">3</span><span class="token punctuation">;</span>
+    <span class="token function">load</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span><span class="token operator">...</span><span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+
+<span class="token keyword">class</span> <span class="token class-name">guide_step_3</span> <span class="token keyword">extends</span> <span class="token class-name">mk</span><span class="token punctuation">.</span>guide_step_base <span class="token punctuation">{</span>
+    step_n <span class="token operator">=</span> <span class="token number">2</span><span class="token punctuation">;</span>
+    <span class="token function">load</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span><span class="token operator">...</span><span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>也就是把步骤2的 step_n 属性改为 3，步骤3的 step_n 属性改为 2</p>`,6);function i(u,d){return n(),a("div",null,[o,e(" more "),l])}const r=s(c,[["render",i],["__file","2.html.vue"]]);export{r as default};
