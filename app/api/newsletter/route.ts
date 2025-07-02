e7541fad93dbec0e407737d7885a641240b2b96e
yuanfengxin <1226085293@qq.com>
2025-07-02 09:57:22 +0800

    关闭路由

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,11 +1,11 @@
-import { NewsletterAPI } from 'pliny/newsletter'
-import siteMetadata from '@/data/siteMetadata'
+// import { NewsletterAPI } from 'pliny/newsletter'
+// import siteMetadata from '@/data/siteMetadata'
 
-export const dynamic = 'force-static'
+// export const dynamic = 'force-static'
 
-const handler = NewsletterAPI({
-  // @ts-ignore
-  provider: siteMetadata.newsletter.provider,
-})
+// const handler = NewsletterAPI({
+//   // @ts-ignore
+//   provider: siteMetadata.newsletter.provider,
+// })
 
-export { handler as GET, handler as POST }
+// export { handler as GET, handler as POST }
